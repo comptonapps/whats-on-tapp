76a49306cd94c94491a10b916f4afc50fab09a11
comptonapps <comptondev@gmail.com>
2022-01-24 13:36:51 -0800

    Add GET, POST, PATCH routes for drink ratings in routes/userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { userIsAuthenticated, checkForCorrectUserOrAdmin } = require('../middleware/auth');
+const { verifyPlace, verifyDrink } = require('../middleware/verify');
 const schemaValidator = require('../helpers/schemaValidator');
-const userCreateSchema = require('../schemata/user/userCreateSchema.json');
 const userUpdateSchema = require('../schemata/user/userUpdateSchema.json');
+const drinkRatingSchema = require('../schemata/drinkRating/drinkRatingSchema.json');
 const Place = require('../models/Place');
+const Drink = require('../models/Drink');
 const PlaceOwner = require('../models/PlaceOwner');
+const PlaceRating = require('../models/PlaceRating');
+const DrinkRating = require('../models/DrinkRating');
 
 
 router.get('/', userIsAuthenticated, async (req, res, next) => {
@@ -24,6 +28,26 @@ router.get('/:user_id', userIsAuthenticated, async (req, res, next) => {
         const user = await User.getById(user_id);
         return res.json({user});
     } catch(e) {
+        return next(e);
+    }
+});
+
+router.get('/:user_id/rating/drink', userIsAuthenticated, async (req, res, next) => {
+    try {
+        const { user_id } = req.params;
+        const drink_ratings = await DrinkRating.getByUserId(user_id);
+        return res.json({drink_ratings});
+    } catch (e) {
+        return next(e);
+    }
+});
+
+router.get('/:user_id/rating/place', userIsAuthenticated, async (req, res, next) => {
+    try {
+        const { user_id } = req.params;
+        const place_ratings = await PlaceRating.getByUserId(user_id);
+        return res.json({place_ratings});
+    } catch (e) {
         return next(e);
     }
 });
@@ -47,6 +71,35 @@ router.post('/:user_id/place', userIsAuthenticated,  async (req, res, next) => {
     }
 });
 
+router.post('/:user_id/rating/place/:place_id', 
+            checkForCorrectUserOrAdmin,
+            verifyPlace, 
+            async (req, res, next) => {
+    try {
+        const { user_id, place_id } = req.params;
+        const { rating } = req.body;
+        const place_rating = await PlaceRating.create(user_id, place_id, rating);
+        return res.status(201).json({ place_rating });
+    } catch(e) {
+        return next(e);
+    }
+});
+
+router.post('/:user_id/rating/drink/:drink_id',
+            checkForCorrectUserOrAdmin,
+            verifyDrink,
+            async (req, res, next) => {
+    try {
+        const { user_id, drink_id } = req.params;
+        const { rating } = req.body;
+        schemaValidator({user_id: +user_id, drink_id: +drink_id, rating}, drinkRatingSchema);
+        const drink_rating = await DrinkRating.create(user_id, drink_id, rating);
+        return res.status(201).json({drink_rating}); 
+    } catch(e) {
+        return next(e);
+    }
+});
+
 router.patch('/:user_id', checkForCorrectUserOrAdmin, async (req, res, next) => {
     try {
         const { user_id } = req.params;
@@ -54,6 +107,35 @@ router.patch('/:user_id', checkForCorrectUserOrAdmin, async (req, res, next) => 
         schemaValidator(userData, userUpdateSchema);
         const user = await User.update(user_id, userData);
         return res.json({user});
+    } catch(e) {
+        return next(e);
+    }
+});
+
+router.patch('/:user_id/rating/place/:place_id', 
+            checkForCorrectUserOrAdmin,
+            verifyPlace, 
+            async (req, res, next) => {
+    try {
+        const { user_id, place_id } = req.params;
+        const { rating } = req.body;
+        const place_rating = await PlaceRating.update(user_id, place_id, rating);
+        return res.json({ place_rating });
+    } catch(e) {
+        return next(e);
+    }
+});
+
+router.patch('/:user_id/rating/drink/:drink_id',
+            checkForCorrectUserOrAdmin,
+            verifyDrink,
+            async (req, res, next) => {
+    try {
+        const { user_id, drink_id } = req.params;
+        const { rating } = req.body;
+        schemaValidator({user_id: +user_id, drink_id: +drink_id, rating}, drinkRatingSchema);
+        const drink_rating = await DrinkRating.update(user_id, drink_id, rating);
+        return res.json({drink_rating}); 
     } catch(e) {
         return next(e);
     }
--- routes/userRoutes.test.js
+++ routes/userRoutes.test.js
@@ -4,12 +4,32 @@ const app = require('../app');
 const db = require('../db');
 const User = require('../models/User');
 const JWT = require('../helpers/JWT');
+const Drink = require('../models/Drink');
+const Place = require('../models/Place');
+const PlaceRating = require('../models/PlaceRating');
+const DrinkRating = require('../models/DrinkRating');
+const { DataCollisionError } = require('../expressError')
 
 let u1;
 let u2;
 
+let drink;
+let drink2;
+
+let place;
+let place2;
+let place3;
+
+let dr1;
+let dr2;
+
+let pr1;
+let pr2;
+
 let token;
 let adminToken;
+
+// TODO: test get ratings routes for drinks and places
 
 beforeAll(async () => {
     const u1Data = {
@@ -34,8 +54,43 @@ beforeAll(async () => {
     u2.created_at = u2.created_at.toISOString();
     u2.updated_at = u2.updated_at.toISOString();
 
+    drink = await Drink.create({name: 'middleman ale', maker: 'test ales, co'});
+    drink2 = await Drink.create({name: 'one eyed jack', maker: '52 pick up brewery'});
+
+    place2 = await Place.create({
+        name: 'Donna\'s Diner',
+        address: '456 Oak Avenue',
+        city: 'Detroit',
+        state: 'MI',
+        zip: '87654'
+    });
+
+    place3 = await Place.create({
+        name: 'Don\'s Pub',
+        address: '456 Pine Avenue',
+        city: 'Madison',
+        state: 'WI',
+        zip: '80654'
+    });
+
+    pr1 = await PlaceRating.create(u2.id, place3.id, 4);
+    pr2 = await PlaceRating.create(u2.id, place2.id, 3);
+    pr1.created_at = pr1.created_at.toISOString();
+    pr1.updated_at = pr1.updated_at.toISOString();
+    pr2.created_at = pr2.created_at.toISOString();
+    pr2.updated_at = pr2.updated_at.toISOString();
+
+    dr1 = await DrinkRating.create(u2.id, drink.id, 5);
+    dr2 = await DrinkRating.create(u2.id, drink2.id, 1);
+    dr1.created_at = dr1.created_at.toISOString();
+    dr1.updated_at = dr1.updated_at.toISOString();
+    dr2.created_at = dr2.created_at.toISOString();
+    dr2.updated_at = dr2.updated_at.toISOString();
+
     token = JWT.getJWT({...u1});
     adminToken = JWT.getJWT({...u2, is_admin: true});
+
+
 });
 
 describe('GET /user', () => {
@@ -90,6 +145,104 @@ describe('GET /user/:user_id', () => {
     });
 });
 
+describe('GET /users/:user_id/rating/drink', () => {
+    test('it should return an array of drink ratings', async () => {
+        const response = await request(app).get(`/user/${u2.id}/rating/drink`).set('authorization', `Bearer ${token}`);
+        expect(response.status).toBe(200);
+        const ratings = response.body.drink_ratings;
+        expect(ratings).toHaveLength(2);
+        expect(ratings).toEqual([dr1, dr2]);
+    });
+});
+
+describe('GET /users/:user_id/rating/place', () => {
+    test('it should return an array of place ratings', async () => {
+        const response = await request(app).get(`/user/${u2.id}/rating/place`).set('authorization', `Bearer ${token}`);
+        expect(response.status).toBe(200);
+        const ratings = response.body.place_ratings;
+        expect(ratings).toHaveLength(2);
+        expect(ratings).toEqual([pr1, pr2]);
+    })
+});
+
+describe('POST /users/:user_id/place', () => {
+    const placeData = {
+        name: 'Rita\'s', 
+        address: '123 Oak Ave', 
+        city: 'Seattle', 
+        state: 'WA', 
+        zip: '98122'
+    }
+    test('it should create a place and a place_owner relationship and return the data', async () => {
+        const response = await request(app).post(`/user/${u1.id}/place`).set('authorization', `Bearer ${token}`).send(placeData);
+        expect(response.status).toBe(201);
+        place = response.body.place;
+        const { place_owner } = response.body;
+        expect(place.id).toEqual(expect.any(Number));
+        expect(place.name).toEqual(placeData.name);
+        expect(place_owner.user_id).toEqual(u1.id);
+        expect(place_owner.place_id).toEqual(place.id);
+    });
+});
+
+describe('POST /users/:user_id/rating/drink/:drink_id', () => {
+    test('it should create a drink rating and return the data', async () => {
+        const response = await request(app).post(`/user/${u1.id}/rating/drink/${drink.id}`)
+                            .set('authorization', `Bearer ${token}`)
+                            .send({rating: 5});
+        expect(response.status).toBe(201);
+        const { drink_rating } = response.body;
+        expect(drink_rating.user_id).toEqual(u1.id);
+        expect(drink_rating.drink_id).toEqual(drink.id);
+        expect(drink_rating.rating).toEqual(5);
+    });
+
+    test('it should return a 404 error code and message for a drink that does not exist', async () => {
+        const response = await request(app).post(`/user/${u1.id}/rating/drink/0`)
+                            .set('authorization', `Bearer ${token}`)
+                            .send({rating: 5});
+        expect(response.status).toBe(404);
+        expect(response.body.message).toEqual('Record not found in drinks');
+    });
+
+    test('it should return a 400 error code and DataCollisionError message for a rating that is already created', async () => {
+        const response = await request(app).post(`/user/${u1.id}/rating/drink/${drink.id}`)
+            .set('authorization', `Bearer ${token}`)
+            .send({rating: 4});
+        expect(response.status).toBe(400);
+        expect(response.body.message).toEqual('Duplicate record already exists');
+    });
+});
+
+describe('POST /users/:user_id/rating/place/:place_id', () => {
+    test('it should create a place rating and return the data', async () => {
+        const response = await request(app).post(`/user/${u1.id}/rating/place/${place.id}`)
+            .set('authorization', `Bearer ${token}`)
+            .send({rating: 4});
+        expect(response.status).toBe(201);
+        const { place_rating } = response.body;
+        expect(place_rating.user_id).toEqual(u1.id);
+        expect(place_rating.place_id).toEqual(place.id);
+        expect(place_rating.rating).toEqual(4);
+    });
+    
+    test('it should return a 404 error code and message for a place that does not exist', async () => {
+        const response = await request(app).post(`/user/${u1.id}/rating/place/0`)
+            .set('authorization', `Bearer ${token}`)
+            .send({rating: 4});
+        expect(response.status).toBe(404);
+        expect(response.body.message).toEqual('Record not found in places');
+    });
+
+    test('it should return a 400 error code and DataCollisionError for a rating that is already created', async () => {
+        const response = await request(app).post(`/user/${u1.id}/rating/place/${place.id}`)
+            .set('authorization', `Bearer ${token}`)
+            .send({rating: 4});
+        expect(response.status).toBe(400);
+        expect(response.body.message).toEqual('Duplicate record already exists');
+    });
+});
+
 describe('PATCH /users/:user_id', () => {
     const updateData = {
         email: 'updateEmail@ue.com',
@@ -123,7 +276,6 @@ describe('PATCH /users/:user_id', () => {
     test('it should respond with a 400 error code for extra fields in the update data object', async () => {
         const response = await request(app).patch(`/user/${u1.id}`).set('authorization', `Bearer ${token}`).send({...updateData, foo: 'bar'});
         expect(response.status).toBe(400);
-        console.log('body', response.body);
     });
 
     test('it should update a user and return the user data for an authenticated user with a matching id', async () => {
@@ -134,6 +286,44 @@ describe('PATCH /users/:user_id', () => {
         expect(updatedUser.email).toEqual(updateData.email);
         expect(updatedUser.city).toEqual(updateData.city);
         expect(updatedUser.state).toEqual(updateData.state);
+    });
+});
+
+describe('PATCH /user/:user_id/rating/drink/:drink_id', () => {
+    test('it should update a drink rating and return the data', async () => {
+        const updatedRating = 1;
+        const response = await request(app).patch(`/user/${u1.id}/rating/drink/${drink.id}`)
+            .set('authorization', `Bearer ${token}`)
+            .send({rating: updatedRating});
+        expect(response.status).toBe(200);
+        expect(response.body.drink_rating.rating).toBe(1);
+    });
+
+    test('it should return a 404 error code for a non-existent rating', async () => {
+        const updatedRating = 1;
+        const response = await request(app).patch(`/user/${u1.id}/rating/drink/0`)
+            .set('authorization', `Bearer ${token}`)
+            .send({rating: updatedRating});
+        expect(response.status).toBe(404);
+    });
+});
+
+describe('PATCH /user/:user_id/rating/place/:place_id', () => {
+    test('it should update a place rating and return the data', async () => {
+        const updatedRating = 1;
+        const response = await request(app).patch(`/user/${u1.id}/rating/place/${place.id}`)
+            .set('authorization', `Bearer ${token}`)
+            .send({rating: updatedRating});
+        expect(response.status).toBe(200);
+        expect(response.body.place_rating.rating).toBe(1);
+    });
+
+    test('it should return a 404 error code for a non-existent rating', async () => {
+        const updatedRating = 1;
+        const response = await request(app).patch(`/user/${u1.id}/rating/place/0`)
+            .set('authorization', `Bearer ${token}`)
+            .send({rating: updatedRating});
+        expect(response.status).toBe(404);
     });
 });
 
@@ -172,5 +362,7 @@ describe('DELETE /users/:user_id', () => {
 
 afterAll(async () => {
     await db.query('DELETE FROM users');
+    await db.query('DELETE FROM places');
+    await db.query('DELETE FROM drinks');
     await db.end();
 });
