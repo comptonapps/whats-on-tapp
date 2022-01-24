6548fa9da0b57a0d3c91334f9b07f4e1d9d2c170
comptonapps <comptondev@gmail.com>
2022-01-24 14:53:35 -0800

    verifyOwnership function add to verify.js to check for valid place_owner relationship.  Tests added to verify.test.js

--- middleware/verify.js
+++ middleware/verify.js
@@ -1,19 +1,44 @@
 const Place = require('../models/Place');
 const Drink = require('../models/Drink');
+const PlaceOwner = require('../models/PlaceOwner');
 
 async function verifyPlace(req, res, next) {
-    const { place_id } = req.params;
-    await Place.getById(place_id);
-    return next();
+    try {
+        const { place_id } = req.params;
+        await Place.getById(place_id);
+        return next();
+    } catch(e) {
+        return next(e)
+    }
 };
 
 async function verifyDrink(req, res, next) {
-    const { drink_id } = req.params;
-    await Drink.getById(drink_id);
-    return next();
+    try {
+        const { drink_id } = req.params;
+        await Drink.getById(drink_id);
+        return next();
+    } catch(e) {
+        return next(e);
+    }
+};
+
+async function verifyOwnership(req, res, next) {
+    try {
+        const user = res.locals.user;
+        let { place_id } = req.params;
+        place_id = +place_id;
+        if (user.is_admin) {
+            return next();
+        }
+        await PlaceOwner.getByIds(user.id, place_id);
+        return next();
+    } catch(e) {
+        return next(e);
+    }
 };
 
 module.exports = {
     verifyPlace,
-    verifyDrink
+    verifyDrink,
+    verifyOwnership
 }
--- middleware/verify.test.js
+++ middleware/verify.test.js
@@ -1,17 +1,25 @@
 const {
     verifyDrink,
-    verifyPlace
+    verifyPlace,
+    verifyOwnership
 } = require('./verify');
 const Drink = require('../models/Drink');
 const Place = require('../models/Place');
+const User = require('../models/User');
+const PlaceOwner = require('../models/PlaceOwner');
 const db = require('../db');
 
+let user;
 let drink;
 let place;
+let placeOwner;
 
 beforeAll(async () => {
+    
     drink = await Drink.create({name: 'middleman ale', maker: 'test ales, co'});
     place = await Place.create({name: 'Rita\'s', address: '123 Oak Ave', city: 'Seattle', state: 'WA', zip: '98122'});
+    user = await User.create({username: 'verifytestuser', password: 'wwwasdwww', city: 'Sacto', state: 'CA', email: 'tupo@zombo.com'});
+    placeOwner = await PlaceOwner.createRelationship(user.id, place.id);
 })
 
 describe('verifyDrink function', () => {
@@ -42,8 +50,45 @@ describe('verifyPlace function', () => {
     });
 });
 
+describe('verifyOwnership function', () => {
+    test('it should return next for valid place_owner ids', async () => {
+        expect.assertions(1);
+        const next = function (err) {
+            expect(err).toBeFalsy();
+        }
+        const locals = { user: {id: +user.id, is_admin: false}}
+        const res = { locals };
+        const req = { params : {place_id: +place.id}}
+        await verifyOwnership(req, res, next);
+    });
+
+    test('it should return next for an admin request', async () => {
+        expect.assertions(1);
+        const next = function (err) {
+            expect(err).toBeFalsy();
+        }
+        const locals = { user: {id: 0, is_admin: true}}
+        const res = { locals };
+        const req = { params : {place_id: +place.id}}
+        await verifyOwnership(req, res, next);
+    });
+
+    test('it should return an error in the next function for bad ids', async () => {
+        expect.assertions(1);
+        const next = function (err) {
+            expect(err).toBeTruthy();
+        }
+        const locals = { user: {id: 0, is_admin: false}}
+        const res = { locals };
+        const req = { params : {place_id: 0}}
+        await verifyOwnership(req, res, next);
+    })
+})
+
 afterAll(async () => {
     await db.query('DELETE FROM drinks');
     await db.query('DELETE FROM places');
+    await db.query('DELETE FROM users');
+    await db.query('DELETE FROM place_owners');
     await db.end();
 });
